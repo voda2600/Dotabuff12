2f35a6cf87e525da0e69181a25b477012fc550db
voda2600@mail.ru <voda2600@mail.ru>
2020-11-28 11:38:00 +0300

    Asp.Net

---
+++ SemestreWork/SemestreWork/Pages/Registration.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SemestreWork.Pages
+{
+    public class RegistrationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ SemestreWork/SemestreWork/Repository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SemestreWork.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+    Task<T> GetByIdAsync(int id);
+
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<int> AddAsync(T entity);
+    Task<int> UpdateAsync(T entity);
+    Task<int> DeleteAsync(int id);
+    }
+}
