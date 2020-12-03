f1e3a9a25be6060b7de8d2b9d056ca397371f26a
voda2600@mail.ru <voda2600@mail.ru>
2020-12-03 20:44:03 +0300

    ProjectReady100%

---
+++ SemestreWork/SemestreWork/Pages/SiteUsers.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SemestreWork.Models;
+using SemestreWork.Repository;
+
+namespace SemestreWork.Pages
+{
+    public class SiteUsersModel : PageModel
+    {
+        private IRegisterRepository _registerRepository;
+        public SiteUsersModel(IRegisterRepository registerRepository)
+        {
+            _registerRepository = registerRepository;
+        }
+        public List<RegisterModel> MyUsers { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public PartialViewResult OnGetUsersPartial()
+        {
+            MyUsers = _registerRepository.GetList();
+            return Partial("_UsersList", MyUsers);
+        }
+    }
+}
